44da48e8a082e64c528dee68fdba9eda7500c7d7
danimosteiro25@gmail.com <danimosteiro25@gmail.com>
2019-10-03 18:55:25 +0200

    intentar tabular

--- Test.java
+++ Test.java
@@ -3,16 +3,16 @@ import java.util.ArrayList;
 public class Test {
     public static void main(String[] args){
         // Casillas UDC Ferrol (SUR)
-        Casilla saida = new Casilla("Saída", "especial", 0, 0);
-        Casilla solar1 = new Casilla("Enx. Eléctrica", "solar", 1, 100000);
-        Casilla caixa1 = new Casilla("Caixa de comunidade", "caixa", 2, 0);
-        Casilla solar2 = new Casilla("Enx. Mecánica", "solar", 3, 100000);
-        Casilla imp1 = new Casilla("Imposto Militar", "imposto", 4,300000);
-        Casilla trans1 = new Casilla("Porto de Ferrol", "transporte", 5, 300000);
-        Casilla solar3 = new Casilla("Podoloxía", "solar", 6, 130000);
-        Casilla sorte1 = new Casilla("Sorte", "sorte", 7, 0);
-        Casilla solar4 = new Casilla("Humanidades", "solar", 8, 130000);
-        Casilla solar5 = new Casilla("Información e Documentación", "solar", 9, 130000);
+        Casilla saida = new Casilla( "Saída\t\t", "especial", 0, 0);
+        Casilla solar1 = new Casilla("Enx. Eléctrica        ", "solar", 1, 100000);
+        Casilla caixa1 = new Casilla("Caixa de comunidade   ", "caixa", 2, 0);
+        Casilla solar2 = new Casilla("Enx. Mecánica         ", "solar", 3, 100000);
+        Casilla imp1 = new Casilla(  "Imposto Militar    ", "imposto", 4,300000);
+        Casilla trans1 = new Casilla("Porto de Ferrol    ", "transporte", 5, 300000);
+        Casilla solar3 = new Casilla("Podoloxía\t", "solar", 6, 130000);
+        Casilla sorte1 = new Casilla("Sorte\t", "sorte", 7, 0);
+        Casilla solar4 = new Casilla("Humanidades\t", "solar", 8, 130000);
+        Casilla solar5 = new Casilla("Documentación\t", "solar", 9, 130000);
 
         // Casillas UDC Coruña (OESTE)
         Casilla carcere = new Casilla("Cárcere", "especial", 10, 0);
@@ -40,61 +40,134 @@ public class Test {
 
         // Casillas USC Santiago (LESTE)
         Casilla irCarcere = new Casilla("Ir ao cárcere", "especial", 30, 0);
-        Casilla solar18 = new Casilla("Farmacia", "solar", 31, 500000);
-        Casilla solar19 = new Casilla("Bioloxía", "solar", 32, 500000);
-        Casilla caixa3 = new Casilla("Caixa de comunidade", "solar", 33, 0);
-        Casilla solar20 = new Casilla("Química", "solar", 34, 500000);
-        Casilla trans4 = new Casilla("Estación de tren de Santiago", "transporte", 35, 300000);
-        Casilla sorte3 = new Casilla("Sorte", "sorte", 36, 0);
-        Casilla solar21 = new Casilla("Física", "solar", 37, 650000);
-        Casilla imp2 = new Casilla("Imposto da luz", "imposto", 38, 150000);
-        Casilla solar22 = new Casilla("ETSE", "solar", 39, 650000);
+        Casilla solar18 = new Casilla(  "Farmacia\t", "solar", 31, 500000);
+        Casilla solar19 = new Casilla(  "Bioloxía\t", "solar", 32, 500000);
+        Casilla caixa3 = new Casilla(   "Caixa Comunidade", "solar", 33, 0);
+        Casilla solar20 = new Casilla(  "Química\t", "solar", 34, 500000);
+        Casilla trans4 = new Casilla(   "E.F.F.C.C Santiago", "transporte", 35, 300000);
+        Casilla sorte3 = new Casilla(   "Sorte\t", "sorte", 36, 0);
+        Casilla solar21 = new Casilla(  "Física\t", "solar", 37, 650000);
+        Casilla imp2 = new Casilla(     "Imposto da luz", "imposto", 38, 150000);
+        Casilla solar22 = new Casilla(  "ETSE\t", "solar", 39, 650000);
 
         //Creamos los grupos
         ArrayList<Casilla> casillasGrupo1 = new ArrayList<>();
         casillasGrupo1.add(solar1);
         casillasGrupo1.add(solar2);
         Grupo grupo1 = new Grupo(casillasGrupo1, 1, "azul", "sur");
+
         ArrayList<Casilla> casillasGrupo2 = new ArrayList<>();
         casillasGrupo2.add(solar3);
         casillasGrupo2.add(solar4);
         casillasGrupo2.add(solar5);
         Grupo grupo2 = new Grupo(casillasGrupo2, 2, "amarillo", "sur");
+
         ArrayList<Casilla> casillasGrupo3 = new ArrayList<>();
         casillasGrupo3.add(solar6);
         casillasGrupo3.add(solar7);
         casillasGrupo3.add(solar8);
         Grupo grupo3 = new Grupo(casillasGrupo3, 3, "violeta", "oeste");
+
         ArrayList<Casilla> casillasGrupo4 = new ArrayList<>();
         casillasGrupo4.add(solar9);
         casillasGrupo4.add(solar10);
         casillasGrupo4.add(solar11);
         Grupo grupo4 = new Grupo(casillasGrupo4, 4, "naranja", "oeste");
+
         ArrayList<Casilla>  casillasGrupo5 = new ArrayList<>();
         casillasGrupo5.add(solar12);
         casillasGrupo5.add(solar13);
         casillasGrupo5.add(solar14);
         Grupo grupo5 = new Grupo(casillasGrupo5,5, "verde", "norte");
+
         ArrayList<Casilla>  casillasGrupo6 = new ArrayList<>();
         casillasGrupo6.add(solar15);
         casillasGrupo6.add(solar16);
         casillasGrupo6.add(solar17);
         Grupo grupo6 = new Grupo(casillasGrupo6,6, "rojo", "norte");
+
         ArrayList<Casilla>  casillasGrupo7 = new ArrayList<>();
         casillasGrupo7.add(solar18);
         casillasGrupo7.add(solar19);
         casillasGrupo7.add(solar20);
         Grupo grupo7 = new Grupo(casillasGrupo7,7, "marron", "leste");
+
         ArrayList<Casilla>  casillasGrupo8 = new ArrayList<>();
         casillasGrupo8.add(solar21);
         casillasGrupo8.add(solar22);
         Grupo grupo8 = new Grupo(casillasGrupo8,8, "gris", "leste");
+
+        //hacemos los grupos de los lados
+        ArrayList<Casilla>  casillasLadoNorte = new ArrayList<>();
+        casillasLadoNorte.add(parking);
+        casillasLadoNorte.add(solar12);
+        casillasLadoNorte.add(sorte2);
+        casillasLadoNorte.add(solar13);
+        casillasLadoNorte.add(solar14);
+        casillasLadoNorte.add(trans3);
+        casillasLadoNorte.add(solar15);
+        casillasLadoNorte.add(solar16);
+        casillasLadoNorte.add(serv2);
+        casillasLadoNorte.add(solar17);
+        casillasLadoNorte.add(irCarcere);
+        Grupo ladoNorte = new Grupo(casillasLadoNorte,9, "norte");
+
+        ArrayList<Casilla>  casillasLadoOeste = new ArrayList<>();
+        casillasLadoOeste.add(solar11);
+        casillasLadoOeste.add(solar10);
+        casillasLadoOeste.add(caixa2);
+        casillasLadoOeste.add(solar9);
+        casillasLadoOeste.add(trans2);
+        casillasLadoOeste.add(solar8);
+        casillasLadoOeste.add(solar7);
+        casillasLadoOeste.add(serv1);
+        casillasLadoOeste.add(solar6);
+        Grupo ladoOeste = new Grupo(casillasLadoOeste,10, "oeste");
+
+        ArrayList<Casilla>  casillasLadoSur = new ArrayList<>();
+        casillasLadoSur.add(saida);
+        casillasLadoSur.add(solar1);
+        casillasLadoSur.add(caixa1);
+        casillasLadoSur.add(solar2);
+        casillasLadoSur.add(imp1);
+        casillasLadoSur.add(trans1);
+        casillasLadoSur.add(solar3);
+        casillasLadoSur.add(sorte1);
+        casillasLadoSur.add(solar4);
+        casillasLadoSur.add(solar5);
+        casillasLadoSur.add(carcere);
+        Grupo ladoSur = new Grupo(casillasLadoSur,12, "sur");
+
+        ArrayList<Casilla>  casillasLadoLeste = new ArrayList<>();
+        casillasLadoLeste.add(solar19);
+        casillasLadoLeste.add(solar20);
+        casillasLadoLeste.add(caixa3);
+        casillasLadoLeste.add(solar21);
+        casillasLadoLeste.add(trans4);
+        casillasLadoLeste.add(sorte3);
+        casillasLadoLeste.add(solar21);
+        casillasLadoLeste.add(imp2);
+        casillasLadoLeste.add(solar22);
+        Grupo ladoLeste = new Grupo(casillasLadoLeste,11, "leste");
 
         Jugador jugador1 = new Jugador("Pepe", "Esfinge", 100000);
         solar1.setDuenho(jugador1);
         System.out.println(solar1.pertence(jugador1));
         Jugador jugador2 = new Jugador("Dani", "Coche", 100000);
         System.out.println(solar1.pertence(jugador2));
+
+        String texto;
+        texto = "";
+        for (Casilla casillas : ladoNorte.getCasillas()){
+            texto += "|" + casillas.getNombre() + "|";
+        }
+        texto += "\n";
+
+        for (Casilla casillas : ladoSur.getCasillas()){
+            texto += "|" + casillas.getNombre() + "|";
+        }
+
+        System.out.println(texto);
 
     }
 }
