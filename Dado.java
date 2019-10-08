9714d2eb348803d23a28526d3f2356552c2ed9fa
danimosteiro25@gmail.com <danimosteiro25@gmail.com>
2019-10-08 19:34:09 +0200

    tirar dados

--- Dado.java
+++ Dado.java
@@ -11,27 +11,26 @@ public class Dado {
     public void generarValorDados(Dado dado) throws InterruptedException {
         Random ale = new Random(System.currentTimeMillis()); //pone una semilla nueva de cada vez
 
-        dado.dado1 = (int) (Math.random()*6 + 1);
+        dado.dado1 = (int) (Math.random() * 6 + 1);
         Thread.sleep(50);
         //ale = new Random(System.currentTimeMillis());
-        dado.dado2 = (int) (Math.random()*6 + 1);
+        dado.dado2 = (int) (Math.random() * 6 + 1);
 
     }
 
     public Dado(Dado dado) throws InterruptedException {
         dado.generarValorDados(dado);
     }
+
+    public int lanzarDados() throws InterruptedException {
+        this.dado1 = (int) Math.floor(Math.random() * 6 + 1);
+        Thread.sleep(50);
+        this.dado2 = (int) Math.floor(Math.random() * 6 + 1);
+        return this.dado1 + this.dado2;
+    }
+
     public String toString() {
-        String texto;
-
-        if (this.dado2 != this.dado1) {
-            texto = String.valueOf(this.dado1 + this.dado2);
-        } else {
-            texto = "O valor do primeiro dado é: " + this.dado1 + "O valor do segundo dado é: " + this.dado2;
-        }
-
-
-        return texto;
+        return "O valor do primeiro dado é: " + this.dado1 + "\nO valor do segundo dado é: " + this.dado2;
     }
 
 
