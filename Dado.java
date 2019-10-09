4ecdb566f9e3d27c55f61865b850c0743a4c3e8c
danimosteiro25@gmail.com <danimosteiro25@gmail.com>
2019-10-09 12:44:57 +0200

    añadir tirardados

--- Dado.java
+++ Dado.java
@@ -22,12 +22,26 @@ public class Dado {
         dado.generarValorDados(dado);
     }
 
-    public int lanzarDados() throws InterruptedException { //editar esta funcion
+    public int lanzarLosDados() throws InterruptedException {
         this.dado1 = (int) Math.floor(Math.random() * 6 + 1);
         Thread.sleep(50);
         this.dado2 = (int) Math.floor(Math.random() * 6 + 1);
         return this.dado1 + this.dado2;
     }
+
+    public void lanzarDados(Jugador jugador, Taboleiro taboleiro) throws InterruptedException {
+        int posActual, posSig;
+        int dado;
+        Casilla casillaSiguiente;
+        dado = lanzarLosDados();
+
+        posActual = jugador.getAvatar().getCasilla().getPosicion();
+        posSig = posActual + dado;
+        casillaSiguiente = taboleiro.getCasillaPosicion(posSig);
+
+        jugador.getAvatar().setCasilla(casillaSiguiente);
+    }
+
 
     public String toString() {
         return "O valor do primeiro dado é: " + this.dado1 + "\nO valor do segundo dado é: " + this.dado2;
