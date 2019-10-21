b8d93ea1b0c6b618f4ca774f741a15027e9edbc2
danimosteiro25@gmail.com <danimosteiro25@gmail.com>
2019-10-21 14:00:06 +0200

    'acabado'

--- Test.java
+++ Test.java
@@ -1,15 +1,9 @@
-import java.util.ArrayList;
+package monopoly;
+
+import monopoly.Menu;
 
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-
-        /*System.out.println(grupo1);
-        Jugador jugador1 = new Jugador("Pepe", "Esfinge", 100000);
-        solar1.setDuenho(jugador1);
-        System.out.println(solar1.pertence(jugador1));
-        Jugador jugador2 = new Jugador("Dani", "Coche", 100000);
-        System.out.println(solar1.pertence(jugador2));
-         */
 
         Menu menu = new Menu();
     }
